c322b64fd5881350092adb2d8c4cfefd499fe1cb
francomuller023 <mullerfranco997@gmail.com>
2023-10-23 19:12:16 -0300

    perros

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 console.log('patos');
 console.log('gatos');
-=======
 console.log('patos');
 console.log('pato1');
 console.log('pato2');
 console.log('pato3');
->>>>>>> adf0c4bf251023a58ba9a3302b18920168282dff
 
 
 console.log("trabajando con github en equipo");
+console.log("perros");
